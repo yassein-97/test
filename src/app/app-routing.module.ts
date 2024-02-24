dfbe2fca938c1b6210d9875099f07f976505d461
yassein-97 <redname.70@yahoo.com>
2024-02-24 12:41:47 +0200

    second build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent,title:'home'},
+  {path:'about',component:AboutComponent,title:'about'},
+  {path:'blog',component:BlogComponent,title:'blog'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
